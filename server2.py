6d31f62f339ed5832e688747a781baa34595f98f
unknown <paula.stochel@gmail.com>
2017-12-06 18:59:21 +0100

    functions get_movie(movie_id) and get_actor(actor_id)

--- server2.py
+++ server2.py
@@ -10,74 +10,7 @@ import json
 BASE_URL = 'api.themoviedb.org'
 API_KEY = 'ef3a8359454d45c5a58ad9e6dc9913e7'
 
-def trial():
-    conn = None
-    try:
-        params = config()
-
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-
-        cur = conn.cursor()
-
-        #try to insert a movie
-        SQL = "INSERT INTO movies (title,year,type) VALUES(%s,%s,%s);"
-        data = ("Hannibal",2001,"Mistery")
-        cur.execute(SQL,data)
-
-        #eliminate a movie
-        #SQL = "DELETE FROM movies WHERE title = 'Hannibal' "
-        #cur.execute(SQL)
-
-        #query to get all from movies
-        print("Querying all movies' content")
-        SQL = "SELECT * FROM movies"
-        cur.execute(SQL)
-        movs = cur.fetchall()
-        print(movs)
-
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.commit()
-            conn.close()
-            print('Database connection closed.')
-
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
- 
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
- 
-        # create a cursor
-        cur = conn.cursor()
-        
- # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
- 
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-       
-     # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-            return "Done"
-
-
+#API
 def request(method, pathname):
     conn = http.client.HTTPSConnection(BASE_URL)
     payload = "{}"
@@ -87,7 +20,7 @@ def request(method, pathname):
     data = res.read()
     return data.decode('utf-8')
 
-
+#DATABASE
 def getconnection():
     conn = None
     try:
@@ -100,33 +33,110 @@ def getconnection():
     finally:
         return conn
 
+#connecting to the database
+conn = getconnection()
+cur = conn.cursor() 
+
+
 def search_movies_by_query(query):
     return request('GET', '/3/search/movie?query={}&'.format(query))
 
-
-def get_movie(movie_title):
-    conn = getconnection()
-    cur = conn.cursor()
-
-    SQL = "SELECT TOP(1) FROM movies WHERE title=%s"
-    cur.execute(SQL,movie_title)
-
-    movie = cur.fetchone()
-
+def add_movie_to_DB(movie_id):   
     movie = request('GET', '/3/movie/{}?'.format(movie_id))
     movie = json.loads(movie)
 
-    #add new movie to database
-    try:
-     # movies.insert_one(movie)
-      print('The movie is added to the database')
-    except BaseException as e:
-      print('Couldn not insert data to the dabase. Response from API: ' + str(movie) + '    ' + str(e))
+    credits = request('GET', '/3/movie/{}/credits?'.format(movie_id))
+    credits = json.loads(credits)
 
-  
-    print('Movie is in the database')
+    director_id = ''
 
- 
+    for attr in credits['crew']:
+        if attr['job'] == 'Director':
+            director_id = attr['id']
+
+    #add movie to database
+    SQL_insert_movie = "INSERT INTO movies (id, title, year, rating, director, length, description, poster_thumb) VALUES ({}, '{}', '{}', {},'{}', {}, '{}', '{}');".format(movie["id"], movie["title"], movie["release_date"], movie["vote_average"], director_id, movie["runtime"], movie["overview"], movie["poster_path"])
+    cur.execute(SQL_insert_movie)
+
+    #add genres
+    SQL_insert_genres = "INSERT INTO genres (movie_id, genre) VALUES ({}, '{}');"
+    for attr in movie['genres']:
+        cur.execute(SQL_insert_genres.format(movie["id"], attr["name"]))
+
+
+    #add people connected with the movie to the database:
+
+    #director
+    if person_not_exists(director_id):
+        add_person_to_DB(director_id)
+        add_movie_person(movie["id"], director_id, "Director")
+
+    #cast
+    for actor in credits['cast']:
+        actor_id = actor['id']
+        if person_not_exists(actor_id):
+            add_person_to_DB(actor_id)
+            add_movie_person(movie_id, actor_id, actor['character'])
+
+
+def person_not_exists(director_id):
+    SQL_select_person_by_id =  "SELECT * FROM people WHERE id={} LIMIT 1;".format(director_id)
+    cur.execute(SQL_select_person_by_id)
+    person = cur.fetchone();
+    return False if person else True
+
+def add_person_to_DB(person_id):
+    person = request('GET', '/3/person/{}?'.format(person_id))
+    person = json.loads(person)
+
+    SQL_insert_person = "INSERT INTO people (id, name, popularity, birthday, place_of_birth, gender) VALUES ({}, '{}', {}, '{}', '{}', {});".format(person["id"], person["name"], person["popularity"], 
+        person["birthday"], person["place_of_birth"], person["gender"])
+    cur.execute(SQL_insert_person)
+    
+def add_movie_person(movie_id, person_id, role):
+    SQL_insert_movie_person = "INSERT INTO movie_person (movie_id, person_id, role_name) VALUES ({}, {}, '{}');".format(movie_id, person_id, role)
+    cur.execute(SQL_insert_movie_person)
+
+
+
+def get_movie(movie_id):
+    SQL_select_movie_by_id = "SELECT * FROM movies WHERE id={} LIMIT 1;".format(movie_id)
+    cur.execute(SQL_select_movie_by_id)
+
+    movie = cur.fetchone()
+
+    if movie:
+        print("movie in database")
+        return movie
+    else:
+        print("adding movie, actors...")
+        add_movie_to_DB(movie_id)
+
+        conn.commit();
+        cur.execute(SQL_select_movie_by_id)
+        movie_details = cur.fetchone()
+        return movie_details
+
+def get_actor(actor_id):
+    SQL_select_person_by_id = "SELECT * FROM people WHERE id={} LIMIT 1;".format(actor_id)
+    cur.execute(SQL_select_person_by_id)
+
+    actor = cur.fetchone()
+    
+    if actor:
+        print("actor in database")
+        return actor
+    else:
+        print("adding actor...")
+        add_person_to_DB(actor_id)
+        conn.commit();
+
+        cur.execute(SQL_select_person_by_id)
+        actor_details = cur.fetchone()        
+        return actor_details
  
 if __name__ == '__main__':
-    trial()
+    get_movie(11457)
+    get_actor(2454)
+    #closing connection
+    cur.close()
